e0bde9c4516182431828a6e4828cf9555c2b7266
Ricardo759486 <jrcuevas@unbosque.edu.co>
2021-11-08 21:04:21 -0500

    Validaciones

--- src/co/edu/unbosque/model/Algoritmo.java
+++ src/co/edu/unbosque/model/Algoritmo.java
@@ -9,35 +9,37 @@ public class Algoritmo {
 //----------------------------------------COEFICIENTES BINOMIALES--------------------------------------------------------
 //--------------------------------------------------------------------------------------------------------------------------
 
-	public int coefBin(int num1, int num2) {
+	public int coefBin(int n, int k) {
 		int i, j;
-		int[][] tabla = new int[num1 + 1][num1 + 1];
+		int[][] tabla = new int[n + 1][n + 1];
 
-		if (num2 == 0 || num2 == num1) {
+		if (k == 0 || k == n) {
 			return 1;
-		} else {
-			for (i = 0; i <= num1; i++) {
+		} else if(k > n){
+			return 0;
+		}else {
+			for (i = 0; i <= n; i++) {
 				tabla[i][0] = 1;
 			}
 
-			for (i = 1; i <= num1; i++) {
+			for (i = 1; i <= n; i++) {
 				tabla[i][1] = i;
 			}
 
-			for (i = 2; i <= num2; i++) {
+			for (i = 2; i <= k; i++) {
 				tabla[i][i] = 1;
 			}
 
-			for (i = 3; i <= num1; i++) {
-				for (j = 2; j < num1; j++) {
-					if (j <= num2) {
+			for (i = 3; i <= n; i++) {
+				for (j = 2; j < n; j++) {
+					if (j <= k) {
 						tabla[i][j] = tabla[i - 1][j - 1] + tabla[i - 1][j];
 					}
 				}
 			}
 		}
 
-		return tabla[num1][num2];
+		return tabla[n][k];
 	}
 
 //--------------------------------------------------------------------------------------------------------------------------	
